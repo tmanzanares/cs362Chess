3b7a4e995bc530187009ebe8ba9e5b5bd5585fe9
tmanzanares <tmanzanares@unm.edu>
2017-02-20 11:00:19 -0700

    more tests

--- Expectation.java
+++ Expectation.java
@@ -8,11 +8,15 @@ import java.util.Random;
 
 public class Expectation {
 
+	//amount off the top / trials
+	//255 / that
+	
 	public static void main(String[] args) {
 
 		int n = 2;
 		int trials = 100;
-		int[] moves = new int[trials];
+		int innerTrials = 5000000;
+		int[] moves = new int[innerTrials];
 		double[] totalTrials = new double[trials];
 		int[][] board; // = new int[n][n];
 		int x;
@@ -28,19 +32,13 @@ public class Expectation {
 		BufferedWriter bw = null;
 		FileWriter fw = null;
 		try {
-			fw = new FileWriter("C:/Users/Tommy/Desktop/expectation.txt", true);
+			fw = new FileWriter("C:/Users/Tommy/Desktop/expectation.txt", false);
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		bw = new BufferedWriter(fw);
-		try {
-			bw.write("did it");
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 		
 		for (int i = 0; i < trials; i++) {
 			n++; //Use when there is another for loop
@@ -54,12 +52,13 @@ public class Expectation {
 			
 			num = n - 2;
 			System.out.println("Starting the " + num + ", " + num + " test.");
-			while (successfulFalls < trials) {
+			while (successfulFalls < innerTrials) {
 
 				if(fell){
 					x = rand.nextInt(n - 2) + 1;
 					y = rand.nextInt(n - 2) + 1;
 					fell = false;
+					steps = 0;
 				}
 				//System.out.println("X: " + x + ",  Y: " + y);
 				movement = rand.nextInt(8) + 1;
@@ -118,7 +117,7 @@ public class Expectation {
 		for (int i = 0; i < totalTrials.length; i++) {
 			totalavg += totalTrials[i];
 			 try {
-				fw.write("Average expectation of " + i + " " + String.valueOf(totalTrials[i]) + "\n");
+				fw.write("Average expectation of " + i + "   " + String.valueOf(totalTrials[i]) + "\n");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -128,7 +127,7 @@ public class Expectation {
 		System.out.println(totalavg);
 		
 		try {
-			bw.flush();
+			//bw.flush();
 			bw.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
